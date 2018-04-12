eaeef3cf7fcfc67de7fe212a81c107b48e9c8c09
Vaughan Jones <vlj7491@gmail.com>
2018-04-13 08:46:56 +1000

    Correct formatting

--- main.tf
+++ main.tf
@@ -13,13 +13,15 @@ data "template_file" "userdata" {
 }
 
 resource "digitalocean_droplet" "controller" {
-  image      = "${var.image}"
-  name       = "controller${count.index + 1}.${var.domain}"
-  region     = "${var.region}"
-  size       = "${var.droplet_size}"
-  ssh_keys   = [
-    "${digitalocean_ssh_key.main.id}"
+  image  = "${var.image}"
+  name   = "controller${count.index + 1}.${var.domain}"
+  region = "${var.region}"
+  size   = "${var.droplet_size}"
+
+  ssh_keys = [
+    "${digitalocean_ssh_key.main.id}",
   ]
+
   user_data  = "${data.template_file.userdata.rendered}"
   monitoring = true
   count      = 3
@@ -31,5 +33,6 @@ resource "digitalocean_record" "controller" {
   name   = "controller${count.index + 1}"
   value  = "${element(digitalocean_droplet.controller.*.ipv4_address,
 count.index)}"
-  count  = 3
+
+  count = 3
 }
